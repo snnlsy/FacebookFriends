f15a2b609949a783f266c69431f12bd18bb7b2e7
Sinan Ulusoy <snnlsy@gmail.com>
2023-03-31 19:28:12 +0300

    init

---
+++ FacebookFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookFriends
+//
+//  Created by Sinan Ulusoy on 31.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
